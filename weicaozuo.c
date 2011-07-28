0aefd50797762e85e6e09348f0ef3f0f283b4a55
liuyu <1101456843@163.com>
2011-07-28 11:35:18 +0800

    shi liu jin zhi zhuan er jin zhi

---
+++ weicaozuo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    unsigned int a= 0x556677aa;
+    unsigned int mask = (1<<31);
+    for(;mask>0; mask >>= 1 )
+   {
+
+        printf("%d",((a & mask)>0)?1:0);
+   }
+
+        printf("\n");
+
+        return 0;
+   }
+
