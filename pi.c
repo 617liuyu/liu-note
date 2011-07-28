df4b8f93f74f2a882d6cf3036f3aba0d926207b0
liuyu <1101456843@163.com>
2011-07-28 10:55:34 +0800

    liuyu

---
+++ pi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+    double a=3.14;
+    float  b=3.14;
+    if (a==b)
+    {
+       printf ("yes!\n");
+                           
+    }
+
+    else
+    {
+       printf ("no!\n");
+
+    }
+       return 0;
+    }
+
+        
+         
+                
+                         
+                             
+
+                               
+                                     
+                                            
+
+                                                 
+                                                  
+                                          
+                                                      
